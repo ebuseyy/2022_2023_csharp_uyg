d8fd756a9e823841a131d072fafa687f4b76cd9b
ebuseyy <eseyyarer@gmail.com>
2022-12-23 20:47:59 +0300

    Add project files.

---
+++ 2022_2023_csharp_uyg/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace _2022_2023_csharp_uyg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Adi = new System.Windows.Forms.TextBox();
+            this.txt_Soyadi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Ogrno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_yas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adi :";
+            // 
+            // txt_Adi
+            // 
+            this.txt_Adi.Location = new System.Drawing.Point(85, 73);
+            this.txt_Adi.Name = "txt_Adi";
+            this.txt_Adi.Size = new System.Drawing.Size(201, 26);
+            this.txt_Adi.TabIndex = 6;
+            // 
+            // txt_Soyadi
+            // 
+            this.txt_Soyadi.Location = new System.Drawing.Point(85, 130);
+            this.txt_Soyadi.Name = "txt_Soyadi";
+            this.txt_Soyadi.Size = new System.Drawing.Size(201, 26);
+            this.txt_Soyadi.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyadi :";
+            // 
+            // txt_Ogrno
+            // 
+            this.txt_Ogrno.Location = new System.Drawing.Point(85, 16);
+            this.txt_Ogrno.Name = "txt_Ogrno";
+            this.txt_Ogrno.Size = new System.Drawing.Size(201, 26);
+            this.txt_Ogrno.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ogr No :";
+            // 
+            // txt_yas
+            // 
+            this.txt_yas.Location = new System.Drawing.Point(85, 188);
+            this.txt_yas.Name = "txt_yas";
+            this.txt_yas.Size = new System.Drawing.Size(201, 26);
+            this.txt_yas.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Yaş :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 98);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 249);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(874, 284);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(510, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 98);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(694, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 98);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(292, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 561);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_yas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Ogrno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Soyadi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Adi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Adi;
+        private System.Windows.Forms.TextBox txt_Soyadi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Ogrno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_yas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 2022_2023_csharp_uyg/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _2022_2023_csharp_uyg
+{
+    public partial class Form1 : Form
+    {
+        // sqlconnection programımızı vt ye bağlar
+        SqlConnection conn = new SqlConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vtislemyap("insert Tbl_Ogrenci (OgrNo,Adi,Soyadi,yas)\r\nvalues ('" + txt_Ogrno.Text + "','" + txt_Adi.Text + "','" + txt_Soyadi.Text + "'," + txt_yas.Text + ")","Kayıt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // .connectionString; vt nin yolunu tanımlamak için kullanılır
+            conn.ConnectionString = "server=DESKTOP-520STTO; database=_2022_2023_1_donem; uid=sa; pwd=12345;";
+            verigetir();
+        }
+
+        public void verigetir()
+        {
+            // vt den verileri getirme işlemini yapamaktadır.
+            SqlDataAdapter da = new SqlDataAdapter("select * from Tbl_Ogrenci", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vtislemyap("update tbl_ogrenci set ogrno='"+txt_Ogrno.Text+"' ,adi='" + txt_Adi.Text + "',soyadi='"+txt_Soyadi.Text+"', yas="+txt_yas.Text+" where ID='" + label5.Text + "'","Güncelleme");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vtislemyap("delete from tbl_ogrenci where ID='" + label5.Text + "'", "Silme");
+        }
+
+        public void vtislemyap(string sqlcumlesi,string mesaj)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sqlcumlesi;
+            cmd.Connection = conn;
+
+            int sonuc = cmd.ExecuteNonQuery();
+            if (sonuc > 0)
+            {
+                MessageBox.Show(mesaj+" işlemi başarılıdır.", "Bilgi");
+                verigetir();
+                temizle();
+            }
+            conn.Close();
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            label5.Text = dataGridView1.Rows[index].Cells["ID"].Value.ToString();
+
+            txt_Ogrno.Text= dataGridView1.Rows[index].Cells["OgrNo"].Value.ToString();
+
+            txt_Adi.Text= dataGridView1.Rows[index].Cells["adi"].Value.ToString();
+
+            txt_Soyadi.Text= dataGridView1.Rows[index].Cells["Soyadi"].Value.ToString();
+
+            txt_yas.Text= dataGridView1.Rows[index].Cells["yas"].Value.ToString();
+
+        }
+
+        public void temizle()
+        {
+            label5.Text = "";
+            txt_Ogrno.Text = "";
+            txt_Adi.Text = "";
+            txt_Soyadi.Text = "";
+            txt_yas.Text = "";
+        }
+    }
+}
